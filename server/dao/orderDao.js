3ef1f011ca6cb16bcb5098d480691fc4f0849446
jiangnan <229142144@qq.com>
2017-08-26 11:54:35 +0800

    merge

--- server/dao/orderDao.js
+++ server/dao/orderDao.js
@@ -44,23 +44,25 @@ class orderDao extends baseDao {
   }
 
   async update(req, res, next) {
-    const {username, date, time_type} = req.body
+    const {username, date, phone, time_type} = req.body
     const {id} = req.params
     const list = await db(this.db).select().where({date, status: 1})
     let array = []
     list.map(item => {
-      if (item.time_type.length > 1) {
-        array=item.time_type.split(',')
-      } else {
-        if (time_type.indexOf(+item.time_type) > -1) {
-          array.push(item.time_type)
+      if (item.id != id) {
+        if (item.time_type.length > 1) {
+          array=item.time_type.split(',')
+        } else {
+          if (time_type.indexOf(+item.time_type) > -1) {
+            array.push(item.time_type)
+          }
         }
       }
     })
     if (array.length) {
       res.status(406).json({msg: '已经有预约信息！请重新选择！', time: array })
     } else {
-      await super.update(req, res, next, {username, date, time_type: time_type.join(',')})
+      await super.update(req, res, next, {username, date, phone, time_type: time_type.join(',')})
     }
   }
 }
