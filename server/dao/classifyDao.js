222c5cf417d462644f24928b68e3cb1d8aa2f6e2
jiangnan <229142144@qq.com>
2017-09-10 20:59:25 +0800

    class add bug

--- server/dao/classifyDao.js
+++ server/dao/classifyDao.js
@@ -31,7 +31,6 @@ class classifyDao extends baseDao {
     super(db, search, orderBy)
   }
   async insert(req, res, next) {
-    const result = await db('recommends').select().innerJoin('products', 'products.id', 'recommends.product_id')
     const [{count}] = await db(this.db).select().where({status: 1}).count('id as count')
     req.body.sort = count +1
     super.insert(req, res, next)
